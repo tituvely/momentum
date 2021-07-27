498e2a942a15359fcdf41a7695eaac63f6b1e03f
Titu YooKyung Kim <tituvely@gmail.com>
2021-07-27 17:59:26 +0900

    4.4 localStorage

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(event);
-}
-
-function handleLinkClick(event) {
-    event.preventDefault();
-    console.dir(event);
-    alert("clicked");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem("username", username);
+    // greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit)
-link.addEventListener("click", handleLinkClick)
+
