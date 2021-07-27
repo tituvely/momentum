851d0547a31e8873e1121476ace252cba7c25abf
Titu YooKyung Kim <tituvely@gmail.com>
2021-07-27 17:06:19 +0900

    4.3 event argument

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
 
-function onLoginSubmit(info) {
-    info.preventDefault();
-    console.log(info);
+const link = document.querySelector("a");
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(event);
+}
+
+function handleLinkClick(event) {
+    event.preventDefault();
+    console.dir(event);
+    alert("clicked");
 }
 
 loginForm.addEventListener("submit", onLoginSubmit)
+link.addEventListener("click", handleLinkClick)
