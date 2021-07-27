0893badc3f03a0dcb32b0bfb3db4433cd8668474
Titu YooKyung Kim <tituvely@gmail.com>
2021-07-27 16:07:23 +0900

    3.6 CSS in JS 2

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    const currentColor = h1.style.color;
-    let newColor;
-    if(currentColor === "blue") {
-        newColor = "tomato";
+    if(h1.className === "active") {
+        h1.className = "";
     } else {
-        newColor = "blue";
+        h1.className = "active";
     }
-    h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
