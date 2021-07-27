1c342174da48d4a10036a7762b51f47cb8079190
Titu YooKyung Kim <tituvely@gmail.com>
2021-07-27 15:59:50 +0900

    3.6 CSS in JS

--- app.js
+++ app.js
@@ -1,40 +1,14 @@
-// const title = document.querySelector(".hello h1:nth-child(2)");
-const title = document.querySelector(".hello h1:first-child");
+const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    title.style.color = "yellow";
-    title.textContent = "Title was clicked!";
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue") {
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
-function handleMouseEnter() {
-    console.log("mouseenter");
-}
-
-function handleMouseLeave() {
-    console.log("mouseleave");
-}
-
-function handleWindowResize() {
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy() {
-    alert("Stop Copy");
-}
-
-function handleWindowOffline() {
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline() {
-    alert("WIFI CONNECTED");
-}
-
-title.onclick = handleTitleClick;
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
