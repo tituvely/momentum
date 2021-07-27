8cdecdcd49f34715f8714946045b447357e713fd
Titu YooKyung Kim <tituvely@gmail.com>
2021-07-27 16:15:48 +0900

    3.6 CSS in JS 3

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick() {
-    if(h1.className === "active") {
-        h1.className = "";
-    } else {
-        h1.className = "active";
-    }
+    const clickedClass = "active";
+    // if(h1.classList.contains(clickedClass)) {
+    //     h1.classList.remove(clickedClass);
+    // } else {
+    //     h1.classList.add(clickedClass);
+    // }
+    h1.classList.toggle(clickedClass);
 }
 
 h1.addEventListener("click", handleTitleClick);
