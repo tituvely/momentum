602db66a642f951ac7c8ae2e8e86962de5d5afe3
Titu YooKyung Kim <tituvely@gmail.com>
2021-07-28 21:40:26 +0900

    weather

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,16 @@
+
+const API_KEY="";
+
 function onGeoOK(position){
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    fetch(url).then(response => response.json()).then(data => {
+        const location = document.querySelector("#weather span:first-child");
+        const weather = document.querySelector("#weather span:last-child");
+        location.innerText = data.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
 }
 
 function onGeoError() {
